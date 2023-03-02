8447a31540135f226e89e23c0a42210e83bdf096
rmznkrts74 <rmznkrts74@gmail.com>
2023-03-02 23:41:20 +0300

    Update
    Tools for brute force and form app for sql injection

---
+++ Form app For Sql injection/formForSuggestion/formForSuggestion/formSuggest.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace formForSuggestion
+{
+    partial class formSuggest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtsuggestion = new System.Windows.Forms.Label();
+            this.txtNameSurname = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name Surname:";
+            // 
+            // txtsuggestion
+            // 
+            this.txtsuggestion.AutoSize = true;
+            this.txtsuggestion.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtsuggestion.Location = new System.Drawing.Point(12, 59);
+            this.txtsuggestion.Name = "txtsuggestion";
+            this.txtsuggestion.Size = new System.Drawing.Size(110, 24);
+            this.txtsuggestion.TabIndex = 1;
+            this.txtsuggestion.Text = "Suggestion:";
+            this.txtsuggestion.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtNameSurname
+            // 
+            this.txtNameSurname.Location = new System.Drawing.Point(168, 21);
+            this.txtNameSurname.Name = "txtNameSurname";
+            this.txtNameSurname.Size = new System.Drawing.Size(203, 22);
+            this.txtNameSurname.TabIndex = 2;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(16, 99);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(593, 302);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(404, 236);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(467, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 42);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.txtNameSurname);
+            this.Controls.Add(this.txtsuggestion);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label txtsuggestion;
+        private System.Windows.Forms.TextBox txtNameSurname;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Form app For Sql injection/formForSuggestion/formForSuggestion/formSuggest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace formForSuggestion
+{
+    public partial class formSuggest : Form
+    {
+        public formSuggest()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string cs = "Data Source=192.168.1.39;INITIAL CATALOG=HR;UID=SA;PWD=Password1;";
+            SqlConnection conn=new SqlConnection(cs);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection=conn;
+            string sql = "insert into hr.dbo.suggestion(NameSurname,Date_,SuggestionText)";
+            sql = sql + " Values('" + txtNameSurname.Text + "',GETDATE(),'" + txtsuggestion.Text + "');";
+                sql = sql + "Select @@IDENTITY";//it give us to identity number of suggestion
+            cmd.CommandText=sql;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                int id;
+                id = Convert.ToInt32(ds.Tables[0].Rows[0][0]);
+                MessageBox.Show("Your Suggestion has been posted. ID: " + id.ToString());
+            }
+
+        }
+    }
+}
